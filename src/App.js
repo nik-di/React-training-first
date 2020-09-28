e581b6be7294fa88e4038c4fe898b28afe037da3
Dmitriy Nikishin <51823084+nik-di@users.noreply.github.com>
2020-09-28 20:29:58 +0300

    Props: done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 import Message from './components/Message/Message';
 
 function App() {
   return (
     <div className="App">
-      <Message />
-      <Message />
+      <Message
+        logo={logo}
+        name="Вася"
+        title="Hello, React!"
+        text="React is awesome lib"
+      />
+      <Message
+        logo={logo}
+        name="Петя"
+        title="Hello, React from Петя!"
+        text="It's cool"
+      />
     </div>
   );
 }
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './Message.css';
-import logo from '../../logo.svg';
 
-function Message() {
+function Message(props) {
+  const { logo, name, title, text } = props;
+
   const date = new Date();
   const displayDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
   return (
     <div className="message">
       <div className="message-header">
         <img src={logo} alt="Alt text" />
         <div className="message-header-text__block">
-          <p className="message-header-title">Ivan</p>
+          <p className="message-header-title">{name}</p>
           <span className="message-header-date">{displayDate}</span>
         </div>
       </div>
       <div className="message-body">
-        <h2 className="message-title">Hi this is title!</h2>
-        <p className="message-text">Lorem ipsum semi-long text ....</p>
+        <h2 className="message-title">{title}</h2>
+        <p className="message-text">{text}</p>
       </div>
     </div>
   );

325725cd86219a17c90b7f0936b57e45cf1ad5cc
Dmitriy Nikishin <51823084+nik-di@users.noreply.github.com>
2020-09-29 21:20:02 +0300

    Array.prototype.map in React: done

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import logo from './logo.svg';
 import Message from './components/Message/Message';
 
 function App() {
+  const data = [
+    { id: 1, name: "Вася", logo, title: "Hello, i am Вася!", text: "React is awesome lib" },
+    { id: 2, name: "Петя", logo, title: "Hello, i am Петя!", text: "React is lib" },
+    { id: 3, name: "Настя", logo, title: "Hello, i am Настя!", text: "World is awesome" },
+    { id: 4, name: "Дима", logo, title: "Hello, i am Дима!", text: "Hey, i am learn React" },
+  ];
+
   return (
     <div className="App">
-      <Message
-        logo={logo}
-        name="Вася"
-        title="Hello, React!"
-        text="React is awesome lib"
-      />
-      <Message
-        logo={logo}
-        name="Петя"
-        title="Hello, React from Петя!"
-        text="It's cool"
-      />
+      {
+        data.map(({ name, logo, text, title, id }) => {
+          return (
+            <Message
+              name={name}
+              logo={logo}
+              text={text}
+              title={title}
+              key={id}
+            />
+          )
+        })
+      }
     </div>
   );
 }

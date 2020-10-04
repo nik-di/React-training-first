c2fa245137fbe61c4fcaaea959df208838e6d931
Dmitriy Nikishin <51823084+nik-di@users.noreply.github.com>
2020-10-04 12:55:06 +0300

    Routing: done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import './App.css';
 import logo from './logo.svg';
 import Message from './components/Message/Message';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
-  const data = [
-    { id: 1, name: "Вася", logo, title: "Hello, i am Вася!", text: "React is awesome lib" },
-    { id: 2, name: "Петя", logo, title: "Hello, i am Петя!", text: "React is lib" },
-    { id: 3, name: "Настя", logo, title: "Hello, i am Настя!", text: "World is awesome" },
-    { id: 4, name: "Дима", logo, title: "Hello, i am Дима!", text: "Hey, i am learn React" },
+  const messageData = [
+    { id: 'id1', name: "Вася", logo, title: "Hello, i am Вася!", text: "React is awesome lib" },
+    { id: 'id2', name: "Петя", logo, title: "Hello, i am Петя!", text: "React is lib" },
+    { id: 'id3', name: "Настя", logo, title: "Hello, i am Настя!", text: "World is awesome" },
+    { id: 'id4', name: "Дима", logo, title: "Hello, i am Дима!", text: "Hey, i am learn React" },
   ];
 
   return (
-    <div className="App">
-      {
-        data.map(({ name, logo, text, title, id }) => {
-          return (
-            <Message
-              name={name}
-              logo={logo}
-              text={text}
-              title={title}
-              key={id}
-            />
-          )
-        })
-      }
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Navigation />
+          <div className="App">
+            {
+              messageData.map(({ name, logo, text, title, id }) => {
+                return (
+                  <Message
+                    name={name}
+                    logo={logo}
+                    text={text}
+                    title={title}
+                    key={id}
+                  />
+                )
+              })
+            }
+          </div>
+        </Route>
+        <Route path='/hello'>
+          <Navigation />
+          <h1>Hello!</h1>
+        </Route>
+        <Route path='/page'>
+          <Navigation />
+          <h1>Page!</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
